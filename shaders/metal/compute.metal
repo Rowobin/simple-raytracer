3a0c8802c28474a6ca1e15b1f0cabd395e6e5062
Rowobin <dockitas152@gmail.com>
2026-06-14 13:07:05 +0100

    simplest raytracer possible better implementation. added glm as a dependency for math operations.

--- shaders/metal/compute.metal
+++ shaders/metal/compute.metal
@@ -3,11 +3,80 @@
 #include <metal_texture>
 using namespace metal;
 
-#line 4 "/Users/rowobin/Documents/GitHub/simple-raytracer/shaders/shaders.slang"
+#line 41 "/Users/rowobin/Documents/GitHub/simple-raytracer/shaders/shaders.slang"
+float rand_0(float seed_0)
+{
+
+#line 42
+    return fract(sin(seed_0 * 12.97999954223632812 + 78.23000335693359375) * 43.54000091552734375);
+}
+
+
+#line 36
+struct Ray_0
+{
+    float3 origin_0;
+    float3 direction_0;
+};
+
+
+#line 36
+Ray_0 Ray_x24init_0(float3 origin_1, float3 direction_1)
+{
+
+#line 36
+    thread Ray_0 _S1;
+    (&_S1)->origin_0 = origin_1;
+    (&_S1)->direction_0 = direction_1;
+
+#line 36
+    return _S1;
+}
+
+
+#line 30
+struct Sphere_0
+{
+    float3 center_0;
+    float radius_0;
+};
+
+
+#line 45
+bool hit_sphere_0(const Sphere_0 thread* sphere_0, const Ray_0 thread* ray_0)
+{
+
+#line 46
+    float3 oc_0 = sphere_0->center_0 - ray_0->origin_0;
+
+    float b_0 = -2.0 * dot(ray_0->direction_0, oc_0);
+
+
+    return (b_0 * b_0 - 4.0 * dot(ray_0->direction_0, ray_0->direction_0) * (dot(oc_0, oc_0) - sphere_0->radius_0 * sphere_0->radius_0)) >= 0.0;
+}
+
+
+#line 4
 struct UniformData_0
 {
+    float r_seed_0;
     int img_width_0;
     int img_height_0;
+    float aspect_ratio_0;
+    float3 camera_position_0;
+    float3 camera_look_at_0;
+    float3 up_0;
+    float focal_length_0;
+    int samples_per_pixel_0;
+    float3 u_0;
+    float3 v_0;
+    float3 w_0;
+    float3 viewport_u_0;
+    float3 viewport_v_0;
+    float3 pixel_u_0;
+    float3 pixel_v_0;
+    float3 pixel00_0;
+    int sphere_count_0;
 };
 
 
@@ -15,23 +84,123 @@ struct UniformData_0
 struct KernelContext_0
 {
     UniformData_0 constant* u_data_0;
+    Sphere_0 device* spheres_0;
     texture2d<float, access::read_write> out_image_0;
 };
 
 
-#line 12 "/Users/rowobin/Documents/GitHub/simple-raytracer/shaders/shaders.slang"
-[[kernel]] void computeMain(uint3 thread_id_0 [[thread_position_in_grid]], UniformData_0 constant* u_data_1 [[buffer(0)]], texture2d<float, access::read_write> out_image_1 [[texture(0)]])
+#line 54 "/Users/rowobin/Documents/GitHub/simple-raytracer/shaders/shaders.slang"
+float4 ray_color_0(const Ray_0 thread* ray_1, KernelContext_0 thread* kernelContext_0)
 {
 
-#line 12
-    thread KernelContext_0 kernelContext_0;
+#line 55
+    float3 norm_0 = normalize(ray_1->direction_0);
 
-#line 12
-    (&kernelContext_0)->u_data_0 = u_data_1;
+#line 55
+    int i_0 = int(0);
 
-#line 12
-    (&kernelContext_0)->out_image_0 = out_image_1;
-    out_image_1.write(float4(float(thread_id_0.x) / float(u_data_1->img_width_0), float(thread_id_0.y) / float(u_data_1->img_height_0), 0.0, 1.0),uint2(int2(thread_id_0.xy)));
+    for(;;)
+    {
+
+#line 57
+        if(i_0 < (kernelContext_0->u_data_0->sphere_count_0))
+        {
+        }
+        else
+        {
+
+#line 57
+            break;
+        }
+
+#line 57
+        thread Sphere_0 _S2 = kernelContext_0->spheres_0[i_0];
+
+#line 57
+        bool _S3 = hit_sphere_0(&_S2, ray_1);
+
+        if(_S3)
+        {
+
+#line 60
+            return float4(1.0, 0.0, 0.0, 1.0);
+        }
+
+#line 57
+        i_0 = i_0 + int(1);
+
+#line 57
+    }
+
+#line 64
+    float a_0 = 0.5 * (norm_0.y + 1.0);
+    return float4((1.0 - a_0))  + float4(a_0)  * float4(0.5, 0.69999998807907104, 1.0, 1.0);
+}
+
+
+
+[[kernel]] void computeMain(uint3 thread_id_0 [[thread_position_in_grid]], UniformData_0 constant* u_data_1 [[buffer(0)]], Sphere_0 device* spheres_1 [[buffer(1)]], texture2d<float, access::read_write> out_image_1 [[texture(0)]])
+{
+
+#line 70
+    thread KernelContext_0 kernelContext_1;
+
+#line 70
+    (&kernelContext_1)->u_data_0 = u_data_1;
+
+#line 70
+    (&kernelContext_1)->spheres_0 = spheres_1;
+
+#line 70
+    (&kernelContext_1)->out_image_0 = out_image_1;
+    float4 _S4 = float4(0.0, 0.0, 0.0, 0.0);
+
+#line 71
+    int i_1 = int(0);
+
+#line 71
+    float4 color_0 = _S4;
+    for(;;)
+    {
+
+#line 72
+        if(i_1 < ((&kernelContext_1)->u_data_0->samples_per_pixel_0))
+        {
+        }
+        else
+        {
+
+#line 72
+            break;
+        }
+
+#line 73
+        float _S5 = float(thread_id_0.x);
+
+#line 73
+        float _S6 = 0.96700000762939453 * float(i_1);
+        float _S7 = float(thread_id_0.y);
+
+#line 74
+        thread Ray_0 _S8 = Ray_x24init_0((&kernelContext_1)->u_data_0->camera_position_0, (&kernelContext_1)->u_data_0->pixel00_0 + float3((_S5 + (rand_0((&kernelContext_1)->u_data_0->r_seed_0 + _S6 * 2.18799996376037598 + _S5 * 0.00300000002607703) - 0.5)))  * (&kernelContext_1)->u_data_0->pixel_u_0 + float3((_S7 + (rand_0((&kernelContext_1)->u_data_0->r_seed_0 + _S6 * 2.11800003051757812 + _S7 + 0.17399999499320984) - 0.5)))  * (&kernelContext_1)->u_data_0->pixel_v_0 - (&kernelContext_1)->u_data_0->camera_position_0);
+
+#line 74
+        float4 _S9 = ray_color_0(&_S8, &kernelContext_1);
+
+
+        float4 color_1 = color_0 + _S9;
+
+#line 72
+        i_1 = i_1 + int(1);
+
+#line 72
+        color_0 = color_1;
+
+#line 72
+    }
+
+#line 80
+    (&kernelContext_1)->out_image_0.write(color_0 / float4(float((&kernelContext_1)->u_data_0->samples_per_pixel_0)) ,uint2(int2(thread_id_0.xy)));
     return;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
 
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
@@ -9,18 +12,54 @@
 #include "imgui_impl_sdl3.h"
 #include "imgui_impl_sdlgpu3.h"
 
+#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+struct Sphere{
+  glm::vec4 center; 
+  float radius;
+  float pad[3];
+};
+
 struct UniformData{
-  int img_width;
-  int img_height;
+  float r_seed;
+  int img_width; 
+  int img_height; 
+  float aspect_ratio; 
+
+  glm::vec4 camera_position; 
+  glm::vec4 camera_look_at; 
+  glm::vec4 up;
+  float focal_length;
+  int samples_per_pixel;
+  float pad2[2];
+
+  glm::vec4 u;
+  glm::vec4 v;
+  glm::vec4 w;
+
+  glm::vec4 viewport_u;
+  glm::vec4 viewport_v;
+  glm::vec4 pixel_u;
+  glm::vec4 pixel_v;
+
+  glm::vec4 pixel00;
+
+  int sphere_count; 
 };
 
 struct Context{
   std::string base_path;
+
   SDL_Window* window;
   SDL_GPUDevice* device;
+
   SDL_GPUComputePipeline* compute_pipeline;
   SDL_GPUTexture* compute_render_texture;
-  UniformData compute_uniform_data;
+  UniformData u_data;
+  SDL_GPUBuffer* compute_sphere_buffer;
+
   Uint64 frame_counter;
   Uint64 last_fps_update;
   Uint64 frame_rate;
@@ -50,9 +89,59 @@ int contextInit(){
   SDL_ClaimWindowForGPUDevice(context.device, context.window);
 
   context.base_path = std::string(SDL_GetBasePath());
+
   context.frame_counter = 0;
   context.last_fps_update = 0;
   context.frame_rate = 0;
+
+  context.u_data.camera_position = {0.0f, 0.0f, 0.0f, 0.0f};
+  context.u_data.camera_look_at = {0.0f, 0.0f, -1.0f, 0.0f};
+  context.u_data.up = {0.0f, 1.0f, 0.0f, 0.0f};
+  context.u_data.focal_length = 1.0f;
+  context.u_data.samples_per_pixel = 20;
+
+  Sphere spheres[] = {
+    { {0.0f, 0.0f, -1.0f, 0.0f}, 0.5f },
+    { {2.0f, 0.0f, -2.0f, 0.0f}, 0.5f },
+    { {-2.0f, 0.0f, -2.0f, 0.0f}, 0.5f },
+    { {0.0f, 0.0f, -4.0f, 0.0f}, 0.5f}
+  };
+  context.u_data.sphere_count = sizeof(spheres) / sizeof(Sphere);
+  Uint32 buffer_size = sizeof(spheres);
+
+  SDL_GPUBufferCreateInfo buffer_info = {
+    .usage = SDL_GPU_BUFFERUSAGE_COMPUTE_STORAGE_READ,
+    .size = buffer_size
+  };
+  context.compute_sphere_buffer = SDL_CreateGPUBuffer(context.device, &buffer_info);
+  if(!context.compute_sphere_buffer){
+    std::cerr << "SDL_CreateGPUBuffer ERROR - " << SDL_GetError() << std::endl;
+    return -1;
+  }
+
+  SDL_GPUTransferBufferCreateInfo transfer_info = {
+    .usage = SDL_GPU_TRANSFERBUFFERUSAGE_UPLOAD,
+    .size = buffer_size
+  };
+  SDL_GPUTransferBuffer* transfer_buffer = SDL_CreateGPUTransferBuffer(context.device, &transfer_info);
+  if(!transfer_buffer){
+    std::cerr << "SDL_CreateGPUTransferBuffer ERROR - " << SDL_GetError() << std::endl;
+    return -1;
+  }
+
+  void* map = SDL_MapGPUTransferBuffer(context.device, transfer_buffer, false);
+  memcpy(map, spheres, buffer_size);
+  SDL_UnmapGPUTransferBuffer(context.device, transfer_buffer);
+
+  SDL_GPUCommandBuffer* command_buffer = SDL_AcquireGPUCommandBuffer(context.device);
+  SDL_GPUTransferBufferLocation transfer_location = {.transfer_buffer = transfer_buffer, .offset = 0};
+  SDL_GPUBufferRegion buffer_region = {.buffer = context.compute_sphere_buffer, .offset = 0, .size = buffer_size};
+  SDL_GPUCopyPass* copy_pass = SDL_BeginGPUCopyPass(command_buffer);
+  SDL_UploadToGPUBuffer(copy_pass, &transfer_location, &buffer_region, false);
+  SDL_EndGPUCopyPass(copy_pass);
+  SDL_SubmitGPUCommandBuffer(command_buffer);
+  SDL_ReleaseGPUTransferBuffer(context.device, transfer_buffer);
+
   return 0;
 }
 
@@ -91,13 +180,50 @@ SDL_GPUComputePipeline* loadComputePipeline(const char* rel_path, const char* en
   return shader_pipeline;
 }
 
+void updateUniformData(){
+  context.u_data.aspect_ratio = (float) context.u_data.img_width / context.u_data.img_height;
+
+  glm::vec3 cam_pos = glm::vec3(context.u_data.camera_position);
+  glm::vec3 cam_look = glm::vec3(context.u_data.camera_look_at);
+  glm::vec3 up = glm::vec3(context.u_data.up);
+
+  glm::vec3 w = glm::normalize(cam_look - cam_pos);
+  glm::vec3 u = glm::normalize(glm::cross(w, up));
+  glm::vec3 v = glm::cross(w, u);
+
+  context.u_data.w = glm::vec4(w, 0.0f);
+  context.u_data.u = glm::vec4(u, 0.0f);
+  context.u_data.v = glm::vec4(v, 0.0f);
+
+  float viewport_w = 4.0f;
+  float viewport_h = viewport_w / context.u_data.aspect_ratio;
+
+  glm::vec3 viewport_u = viewport_w * u;
+  glm::vec3 viewport_v = viewport_h * v;
+
+  context.u_data.viewport_u = glm::vec4(viewport_u, 0.0f);
+  context.u_data.viewport_v = glm::vec4(viewport_v, 0.0f);
+
+  glm::vec3 pixel_u = viewport_u / (float) context.u_data.img_width;
+  glm::vec3 pixel_v = viewport_v / (float) context.u_data.img_height;
+
+  context.u_data.pixel_u = glm::vec4(pixel_u, 0.0f);
+  context.u_data.pixel_v = glm::vec4(pixel_v, 0.0f);
+
+  glm::vec3 pixel00 = cam_pos + context.u_data.focal_length * w;
+  pixel00 -= (viewport_u * 0.5f + viewport_v * 0.5f);
+  pixel00 += pixel_u * 0.5f + pixel_v * 0.5f;
+
+  context.u_data.pixel00 = glm::vec4(pixel00, 0.0f);
+}
+
 SDL_GPUTexture* createScreenSizeRenderTexture(){
-  SDL_GetWindowSizeInPixels(context.window, &context.compute_uniform_data.img_width, &context.compute_uniform_data.img_height);
+  SDL_GetWindowSizeInPixels(context.window, &context.u_data.img_width, &context.u_data.img_height);
   SDL_GPUTextureCreateInfo texture_info = {
     .format = SDL_GPU_TEXTUREFORMAT_R8G8B8A8_UNORM,
     .type = SDL_GPU_TEXTURETYPE_2D,
-    .width = static_cast<Uint32>(context.compute_uniform_data.img_width),
-    .height = static_cast<Uint32>(context.compute_uniform_data.img_height),
+    .width = static_cast<Uint32>(context.u_data.img_width),
+    .height = static_cast<Uint32>(context.u_data.img_height),
     .layer_count_or_depth = 1,
     .num_levels = 1,
     .usage = SDL_GPU_TEXTUREUSAGE_SAMPLER | SDL_GPU_TEXTUREUSAGE_COMPUTE_STORAGE_WRITE,
@@ -111,16 +237,18 @@ SDL_GPUTexture* createScreenSizeRenderTexture(){
 }
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv){ 
+  std::srand(std::time({}));
+
   if(contextInit() != 0){
     return SDL_APP_FAILURE;
   }
 
 #ifdef __APPLE__
   context.compute_pipeline = loadComputePipeline("shaders/metal/compute.metal", "computeMain", SDL_GPU_SHADERFORMAT_MSL,
-      0, 0, 0, 1, 0, 1, 8, 8, 1);
+      0, 0, 1, 1, 0, 1, 8, 8, 1);
 #else
   context.compute_pipeline = loadComputePipeline("shaders/spirv/compute.spv", "main", SDL_GPU_SHADERFORMAT_SPIRV, 
-      0, 0, 0, 1, 0, 1, 8, 8, 1);
+      0, 0, 1, 1, 0, 1, 8, 8, 1);
 #endif
 
   if(!context.compute_pipeline){
@@ -131,6 +259,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv){
   if(!context.compute_render_texture){
     return SDL_APP_FAILURE;
   }
+  updateUniformData();
 
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
@@ -155,12 +284,30 @@ SDL_AppResult SDL_AppIterate(void* appstate){
     context.last_fps_update = time;
   }
 
+  context.u_data.r_seed = std::rand() % 1000;
+
   ImGui_ImplSDLGPU3_NewFrame();
   ImGui_ImplSDL3_NewFrame();
   ImGui::NewFrame();
 
   ImGui::Begin("Controls");
   ImGui::Text("FPS: %llu", context.frame_rate);
+  if(ImGui::CollapsingHeader("Camera", ImGuiTreeNodeFlags_DefaultOpen)){
+    ImGui::SeparatorText("Position");
+    if(ImGui::DragFloat3("##pos", &context.u_data.camera_position[0], 0.1f)){
+      updateUniformData();
+    }
+    ImGui::SeparatorText("Look at");
+    if(ImGui::DragFloat3("##look", &context.u_data.camera_look_at[0], 0.1f)){
+      updateUniformData();
+    }
+    ImGui::SeparatorText("Focal length");
+    if(ImGui::DragFloat("##focal", &context.u_data.focal_length, 0.1f, 0.1f, 5.0f)){
+      updateUniformData();
+    }
+    ImGui::SeparatorText("Samples per pixel");
+    ImGui::DragInt("##samples", &context.u_data.samples_per_pixel, 1, 1, 50);
+  }
   ImGui::End();
 
   ImGui::Render();
@@ -204,7 +351,9 @@ SDL_AppResult SDL_AppIterate(void* appstate){
   }
 
   SDL_BindGPUComputePipeline(compute_pass, context.compute_pipeline);
-  SDL_PushGPUComputeUniformData(command_buffer, 0, &context.compute_uniform_data, sizeof(context.compute_uniform_data));
+  SDL_GPUBuffer* buffers[] = {context.compute_sphere_buffer};
+  SDL_BindGPUComputeStorageBuffers(compute_pass, 0, buffers, 1);
+  SDL_PushGPUComputeUniformData(command_buffer, 0, &context.u_data, sizeof(UniformData));
   SDL_DispatchGPUCompute(compute_pass, width / 8, height / 8, 1);
 
   SDL_EndGPUComputePass(compute_pass);
@@ -268,6 +417,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event){
     if(!context.compute_render_texture){
       return SDL_APP_FAILURE;
     }
+    updateUniformData();
   }
 
   return SDL_APP_CONTINUE;
@@ -278,6 +428,7 @@ void SDL_AppQuit(void* appstate, SDL_AppResult result){
   ImGui_ImplSDL3_Shutdown();
   ImGui::DestroyContext();
 
+  SDL_ReleaseGPUBuffer(context.device, context.compute_sphere_buffer);
   SDL_ReleaseGPUTexture(context.device, context.compute_render_texture);
   SDL_ReleaseGPUComputePipeline(context.device, context.compute_pipeline);
   SDL_DestroyGPUDevice(context.device);
